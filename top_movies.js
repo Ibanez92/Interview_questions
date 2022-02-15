6c72eb2024907d3ba8e906f01fdde3e5b13956ec
Javier Ibanez <expertprep2015@gmail.com>
2022-02-15 16:10:53 -0500

    updated problem 1 on in the top movies section

--- top_movies.js
+++ top_movies.js
@@ -1,35 +1,35 @@
-const parse = require('csv-parse/sync').parse
+const parse = require("csv-parse/sync").parse;
 const fs = require("fs");
-const topMovieCsv = fs.readFileSync("top_movies.csv").toString("utf-8")
+const topMovieCsv = fs.readFileSync("top_movies.csv").toString("utf-8");
 
 const records = parse(topMovieCsv, {
-    columns: true,
-    skip_empty_lines: true
-  });
+  columns: true,
+  skip_empty_lines: true,
+});
+// console.log(JSON.stringify(records, null, 2));
+// function properPropertyTransformer(object) {
+//   const newObj = {
+//     title: object["Title"],
+//     distributor: object["Distributor"],
+//     releaseDate: object["Release Date"],
+//     usSales: object["US Sales"],
+//     worldSales: object["World Sales"],
+//     runtime: object["Runtime"],
+//     rating: object["Rating"],
+//   };
 
-function properPropertyTransformer(object) {
-   const newObj = {
-    title: object['Title'],
-    distributor: object["Distributor"],
-    releaseDate: object['Release Date'],
-    usSales: object['US Sales'],
-    worldSales: object["World Sales"],
-    runtime: object['Runtime'],
-    rating: object["Rating"],
-    }
-
-    return newObj
-}
-
-const modifiedRecordsArr = []
-// for (let i = 0; i < records.length; i++) {
-//   modifiedRecordsArr.push(properPropertyTransformer(records[i]))
-  
+//   return newObj;
 // }
 
-records.forEach(element => {
-  modifiedRecordsArr.push(properPropertyTransformer(element))
-});
+// const modifiedRecordsArr = [];
+// // for (let i = 0; i < records.length; i++) {
+// //   modifiedRecordsArr.push(properPropertyTransformer(records[i]))
+
+// // }
+
+// records.forEach((obj) => {
+//   modifiedRecordsArr.push(properPropertyTransformer(obj));
+// });
 
 // const modifiedRecordsArr = records.map((element) => {
 //   return properPropertyTransformer(element)
@@ -41,18 +41,51 @@ records.forEach(element => {
 
 // Loop through the objects in the array and find the property distributor value
 
-
+// Rick modified 2/15/22 version
+// function matchDistributor(distributor) {
+//   const results = []
+//   records.forEach( element => {
+//     // if that value is set to 'Dreamworks'
+//     // console.log(element);
+//     if(element["Distributor"].toLowerCase() === distributor.toLowerCase()) {
+//       // then push that current object title value into the empty results array
+//       results.push(element.Title)
+//     }
+//   })
+//   return results
+// }
 
 function matchDistributor(distributor) {
-  const results = []
-  modifiedRecordsArr.forEach( element => {
+  return records.filter((obj) => {
+    return obj["Distributor"].toLowerCase() === distributor.toLowerCase()
+  }).map( (object) => {
     // if that value is set to 'Dreamworks'
-    if(element.distributor.toLowerCase() === distributor.toLowerCase()) {
+    // console.log(object);
+    
       // then push that current object title value into the empty results array
-      results.push(element.title)
-    }
+      return object.Title
+    
   })
-  return results
 }
 
-console.log(matchDistributor('Warner Bros.'));
+// function matchDistributor(distributor) {
+//   const results = [];
+//   modifiedRecordsArr.map((element) => {
+//     // if that value is set to 'Dreamworks'
+//     if (returnTitle(element)) {
+//       // then push that current object title value into the empty results array
+//       results.push(element.title);
+//     }
+//   });
+//   return results;
+
+//   function returnTitle(movieObj) {
+//     if (movieObj.distributor.toLowerCase() === distributor.toLowerCase()) {
+//       return movieObj.title;
+//     }
+//   }
+// }
+
+console.log(matchDistributor("Warner Bros."));
+
+// What is the highest grossing moving from Universal Pictures, domestically?
