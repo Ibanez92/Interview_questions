0f71a1952867872558bb95e53688325f2f8d04a4
Javier Ibanez <expertprep2015@gmail.com>
2022-02-14 19:24:55 -0500

    worked on problem in the top movies section and updated readme with newer problems

--- top_movies.js
+++ top_movies.js
@@ -21,4 +21,38 @@ function properPropertyTransformer(object) {
     return newObj
 }
 
-console.log(records);
+const modifiedRecordsArr = []
+// for (let i = 0; i < records.length; i++) {
+//   modifiedRecordsArr.push(properPropertyTransformer(records[i]))
+  
+// }
+
+records.forEach(element => {
+  modifiedRecordsArr.push(properPropertyTransformer(element))
+});
+
+// const modifiedRecordsArr = records.map((element) => {
+//   return properPropertyTransformer(element)
+// })
+
+// console.log(modifiedRecordsArr);
+
+// What movies on this list were distributed by DreamWorks?
+
+// Loop through the objects in the array and find the property distributor value
+
+
+
+function matchDistributor(distributor) {
+  const results = []
+  modifiedRecordsArr.forEach( element => {
+    // if that value is set to 'Dreamworks'
+    if(element.distributor.toLowerCase() === distributor.toLowerCase()) {
+      // then push that current object title value into the empty results array
+      results.push(element.title)
+    }
+  })
+  return results
+}
+
+console.log(matchDistributor('Warner Bros.'));
