38dbf5b4b408874c62f13c3d3674f9602db3bec3
Javier Ibanez <expertprep2015@gmail.com>
2022-03-02 20:26:45 -0500

    working on parsing my string in the stack array

--- stackpractice.js
+++ stackpractice.js
@@ -20,6 +20,27 @@ function pop() {
   return lastItem;
 }
 
+/**
+ * 
+ * @param {string} str - the string to parse 
+ */
+function processString(str) {
+  const tokens = str.split(' ');
+  for (token of tokens) {
+    // parse each token, is this an operator or a number?
+    let parsed = parse
+    // if number, push
+    if(token === typeof Number){
+
+    }
+    // else if operator, pop two then push result back on
+    
+  }
+
+  // at the end, there should just be one item on the stack, the answer
+}
+
+
 push(1);
 console.log(stack);
 push(2);
