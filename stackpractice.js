f0ea40b1dce5c1b792cbd34d7a7a10695c537444
Javier Ibanez <expertprep2015@gmail.com>
2022-03-04 17:16:55 -0500

    went over stack logic and understanding with scott s.

--- stackpractice.js
+++ stackpractice.js
@@ -1,10 +1,15 @@
 let stack = [];
 
+
+
+// [1, 12, ]
+// [13, 2, ]
+
 function push(item) {
   // to do: push onto the stack
   stack[stack.length] = item;
 }
-
+// [1, 3, 4, ]
 function pop() {
   // to do: pop off the stack
   let newStack = [];
@@ -12,28 +17,39 @@ function pop() {
   for (let i = 0; i <= stack.length - 1; i++) {
     if (i < stack.length - 1) {
       newStack[i] = stack[i];
+      // newStack[1, 3, ]
     } else {
       lastItem = stack[i];
+      // lastItem = 4
     }
   }
   stack = newStack;
+  // [1, 3]
   return lastItem;
+  // return 4
 }
 
 /**
  * 
  * @param {string} str - the string to parse 
  */
+
 function processString(str) {
   const tokens = str.split(' ');
   for (token of tokens) {
     // parse each token, is this an operator or a number?
-    let parsed = parse
-    // if number, push
-    if(token === typeof Number){
+    let parsed = parseInt(token) 
+
+    
+    // if operator, pop two then push result back on
+    if(isNaN(parsed)){
+      // let poppedFirst = pop()
+      // let poppedSecond = pop()
+    } else {
+      // if number, push
 
     }
-    // else if operator, pop two then push result back on
+    
     
   }
 
