c3a49a8242150e3dcdc360e12b204f51c4a9a881
Javier Ibanez <expertprep2015@gmail.com>
2022-03-17 15:08:31 -0400

    finished part 1 of rot solver

--- rot_solver.js
+++ rot_solver.js
@@ -4,10 +4,10 @@ function rot(str, shiftValue){
         const charCode = str.charCodeAt(i)
         // if an uppercase letter
         if(charCode >= 65 && charCode <= 90){
-            result += String.fromCharCode(charCode + shiftValue % 26)
+            result += String.fromCharCode(((charCode - 65 + shiftValue) % 26) + 65)
         } // if a lowercase letter 
         else if (charCode >= 97 && charCode <= 122) {
-             result += String.fromCharCode(charCode + shiftValue % 26)
+             result += String.fromCharCode(((charCode - 97 + shiftValue) % 26) + 97)
         } else {
             // if its not a letter, we just include it in the string as is
             result += str[i]
@@ -22,12 +22,14 @@ function rot(str, shiftValue){
     return result
 }
 
-console.log(rot("HELLO", 1));
-console.log(rot("HELLO", 2));
-console.log(rot("HELLO", -1));
-console.log(rot("HELLO", 27));
-console.log(rot("HELLO", 12));
-console.log(rot("ABab", 12));
-console.log(rot("YyZz", 12));
-console.log(rot("OoPp", 12));
-console.log(rot("OoPp, Hello", 12));
+// console.log(rot("HELLO", 1));
+// console.log(rot("HELLO", 2));
+// console.log(rot("HELLO", -1));
+// console.log(rot("HELLO", 27));
+
+// console.log(rot("HELLO", 12));
+// console.log(rot("ABab", 12));
+// console.log(rot("YyZz", 12));
+console.log(rot("Hello, Rick", 1));
+console.log(rot(rot("Hello, Rick", 1), -1));
+// console.log(((121 - 97 + 12) % 26) + 97);
