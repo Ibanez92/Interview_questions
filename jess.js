e52a9994c87874a6c2ac2f66b3803c3738c17549
Javier Ibanez <expertprep2015@gmail.com>
2022-03-20 15:02:23 -0400

    worked on algo problems with jessica mckellar

--- jess.js
+++ jess.js
@@ -21,7 +21,7 @@ function wordsMadeFromLetters(string) {
       let currentLetter = currentWord[j];
 
       // if the input string includes the current letter then continue to the next letter in the word, if we are at the end of the word and the words letters are included in our input string, then put that word in our matchedWords array
-      if (string.includes(currentLetter)) {
+      if (allowedLetters.includes(currentLetter)) {
         if (j + 1 === currentWord.length) {
           matchedWords.push(currentWord);
         }
---
+++ jess2.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const scrabbleFile = fs.readFileSync("sowpods.txt").toString("utf-8");
+const scrabArr = scrabbleFile.split("\n");
+
+// input: array of scrabble words
+// output: ["THIRTEENTH", "TH_TH"]
+function findMatch(scrabArr) {
+    const result = []
+    for (let i = 0; i < scrabArr.length; i++) {
+        if(scrabArr[i].startsWith("TH") && scrabArr[i].endsWith("TH")){
+            result.push(scrabArr[i])
+        }
+    }
+    return result
+}
+
+// console.log(findMatch(scrabArr));
+
+// input: "prefix"
+// output: ["word with prefix", "word with prefix"]
+
+function prefixMatch(prefix) {
+    const result = []
+    for (let i = 0; i < scrabArr.length; i++) {
+        if(scrabArr[i].startsWith(prefix))
+        result.push(scrabArr[i])
+    }
+    return result
+}
+
+// console.log(prefixMatch("THREE"));
+
+// input: "word", "letter"
+// output: a count of how many times letter occurs in word.
+
+function letterCounter(word, letter){
+    let count = 0
+    for (let i = 0; i < word.length; i++) {
+        
+        if(word[i].includes(letter)){
+            count += 1
+        }
+    }
+    return count
+}
+// console.log(letterCounter("REDRR", "R"));
+
+// input: "ABCD"
+// output: ["CAB", "DAD"]
+
+function findMatch2(availableLetters) {
+    const results = []
+    const allowedLetters = availableLetters.split("")
+    for (let i = 0; i < scrabArr.length; i++) {
+        const currentWord = scrabArr[i]
+        for (let j = 0; j < currentWord.length; j++) {
+            const currentLetter = currentWord[j]
+            if(allowedLetters.includes(currentLetter)){
+                if(j + 1 === currentWord.length){
+                    results.push(currentWord)
+                }
+                continue
+            } else {
+                break
+            }
+        }
+    }
+    return results
+}
+// console.log(findMatch2("ABCD"));
+
+
+// input: scrabble words array
+// output: ["ART", "BAT", "CAT"]
+function oneVowelWords(scrabArr){
+    
+}
+console.log(oneVowelWords(scrabArr));
